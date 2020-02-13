8386f4c255953ff83c0bbe1851637369afb264c0
Dominik Grzegorz SYSOJEW - OSINSKI <v8073331@linux22.scm.tees.ac.uk>
2020-02-13 17:37:38 +0000

    included the previously missing files

---
+++ app/src/main/java/uk/ac/tees/v8036651/mode/HomeScreenActivity.java
@@ -0,0 +1,15 @@
+package uk.ac.tees.v8036651.mode;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+        //Test
+        //Commit
+    }
+}
