0e2df501fd8aafbe86b2ed5af3273227756404d1
Patryk NETER <v8243060@linux15.scm.tees.ac.uk>
2020-02-21 11:22:54 +0000

    Added TextWatcher and depenencies for adding buttons to toolbar

--- app/src/main/java/uk/ac/tees/v8036651/mode/IdeScreen.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/IdeScreen.java
@@ -2,19 +2,52 @@ package uk.ac.tees.v8036651.mode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.widget.TextView;
+import androidx.appcompat.widget.Toolbar;
+
+import uk.ac.tees.v8036651.mode.plugins.PluginManager;
 
 public class IdeScreen extends AppCompatActivity {
 
+    @androidx.annotation.RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ide_screen);
 
-        TextView txtCode = (TextView) findViewById(R.id.txtCode);
-        //apparently Horizontal Scrolling in XML is not working. Thanks Android
-        txtCode.setHorizontallyScrolling(true);
 
+        TextView txtCode = (TextView) findViewById(R.id.txtCode);
+
+        txtCode.addTextChangedListener(new TextWatcher() {
+
+            boolean ignore = false;
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(ignore) {
+                    return;
+                }
+
+                ignore = true;
+                PluginManager.formatText((TextView) findViewById(R.id.txtCode), null);
+                ignore = false;
+            }
+        });
+
+        txtCode.setHorizontallyScrolling(true);
     }
 }
