2ba47ef64f6ad57035840cf38bab42b3f0c4891e
Dominic THORP <v8077971@linux13.scm.tees.ac.uk>
2020-02-21 12:29:29 +0000

    Added in the ability to save a file. Bug: files saved are 0B in size

--- app/src/main/java/uk/ac/tees/v8036651/mode/GitHubScreen.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/GitHubScreen.java
@@ -1,12 +1,16 @@
 package uk.ac.tees.v8036651.mode;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.DocumentsContract;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class GitHubScreen extends AppCompatActivity {
+
+    private static final int CREATE_FILE = 1;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -15,7 +19,23 @@ public class GitHubScreen extends AppCompatActivity {
     }
 
     public void doSaveActivity(View view){
-        Intent intent = new Intent(this, SaveActivity.class);
-        startActivity(intent);
+        saveActivity();
+    }
+
+    public void saveActivity(){
+        System.out.println("It entered");
+        Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/plain");
+
+        intent.putExtra(Intent.EXTRA_TEXT, "Testing the file" );
+        intent.putExtra(Intent.EXTRA_TITLE, "invoice");
+
+        // Optionally, specify a URI for the directory that should be opened in
+        // the system file picker when your app creates the document.
+        //intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, pickerInitialUri);
+
+        startActivityForResult(intent, CREATE_FILE);
+        System.out.println("Check storage");
     }
 }
