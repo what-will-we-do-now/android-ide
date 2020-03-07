16b5c5723c9fb6fb4e07073d3fd7014b08833519
vakus <sysojewosinskidominik@gmail.com>
2020-03-07 19:48:37 +0000

    Added JGit dependency, removed old GIT code

--- app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Splash.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Splash.java
@@ -17,8 +17,6 @@ public class Screen_Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        initGit();
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen__splash);
 
@@ -27,55 +25,14 @@ public class Screen_Splash extends AppCompatActivity {
             mainCodeDirectory.mkdir();
         }
 
-            new Handler().postDelayed(new Runnable()
+        new Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run()
             {
-                @Override
-                public void run()
-                {
-                    Intent splashIntent = new Intent(Screen_Splash.this, Screen_Login.class);
-                    startActivity(splashIntent);
-                    finish();
-                }},SPLASH_TIME_OUT);
-        }
-
-    private void initGit(){
-
-        //TODO: temporary move later to spalsh screen
-
-        File gitFile = getFileStreamPath("git");
-
-        if(!gitFile.exists()) {
-
-            System.out.println("Creating GIT file");
-            try {
-
-                InputStream gitIn;
-                if(System.getProperty("os.arch").startsWith("arm")) {
-                    //the current phone is an ARM phone
-                    gitIn = getResources().openRawResource(getResources().getIdentifier("git_arm", "raw", getPackageName()));
-                }else if(System.getProperty("os.arch").equals("i686")){
-                    //the current phone is an x86 phone
-
-                    return;
-                }else{
-
-                    return;
-                }
-                byte[] buffer = new byte[gitIn.available()];
-                gitIn.read(buffer);
-                gitIn.close();
-
-                FileOutputStream gitOut = openFileOutput("git", Context.MODE_PRIVATE);
-                gitOut.write(buffer);
-                gitOut.close();
-
-                File file = getFileStreamPath("git");
-                file.setExecutable(true);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+                Intent splashIntent = new Intent(Screen_Splash.this, Screen_Login.class);
+                startActivity(splashIntent);
+                finish();
+            }},SPLASH_TIME_OUT);
     }
-    }
-
+}
