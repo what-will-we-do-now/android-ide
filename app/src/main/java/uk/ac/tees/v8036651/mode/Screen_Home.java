0c33cdd60bd3645c0587835be577967a5f1cbdc8
vakus <sysojewosinskidominik@gmail.com>
2020-03-09 06:21:19 +0000

    added clone from git

--- app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Home.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Home.java
@@ -1,10 +1,21 @@
 package uk.ac.tees.v8036651.mode;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import uk.ac.tees.v8036651.mode.GitTools.GitCloneTask;
 
 public class Screen_Home extends AppCompatActivity {
     @Override
@@ -28,5 +39,42 @@ public class Screen_Home extends AppCompatActivity {
     public void openSettings(View view){
         Intent settingsIntent = new Intent(this, Screen_Settings.class);
         startActivity(settingsIntent);
+    }
+
+    public void gitClone(View view){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        View dialogue = LayoutInflater.from(this).inflate(R.layout.dialog_git_clone, null);
+
+        builder.setView(dialogue);
+
+        final EditText projectName = dialogue.findViewById(R.id.git_clone_project_name);
+        final EditText gitLink = dialogue.findViewById(R.id.git_clone_url);
+
+        final File parentFile = new File(getExternalFilesDir(null), "MoDE_Code_Directory");
+
+
+        builder.setPositiveButton("Clone", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //clone the git project
+                try {
+                    GitCloneTask gt = new GitCloneTask(new File(parentFile, projectName.getText().toString()), Screen_Home.this, new URL(gitLink.getText().toString()));
+                    gt.execute();
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                    Toast.makeText(Screen_Home.this, R.string.git_clone_error, Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //user pressed cancel
+            }
+        });
+
+        builder.show();
     }
 }
