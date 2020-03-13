70e49119c05949092cc65eea8a4e6dd9797ae15c
vakus <sysojewosinskidominik@gmail.com>
2020-03-13 05:27:33 +0000

    Added project choose screen, Added project creation dialog, Added return to code button

--- app/src/main/java/uk/ac/tees/v8036651/mode/Projects/Project.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/Projects/Project.java
@@ -5,13 +5,21 @@ import android.util.Log;
 import org.eclipse.jgit.api.Git;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 public class Project {
+
+    public static Project openedProject;
 
     private String name;
     private File root;
     private Git git;
+
+    private File lastFile;
+    private Properties prop;
 
     public Project(String name, File root) {
         this(name, root, null);
@@ -30,6 +38,19 @@ public class Project {
         }else {
             this.git = git;
         }
+
+        prop = new Properties();
+
+        try {
+            prop.loadFromXML(new FileInputStream(new File(root, "project.settings")));
+
+            if(prop.getProperty("lastFile") != null) {
+                lastFile = new File(root, prop.getProperty("lastFile"));
+            }
+
+        } catch (IOException e) {
+            Log.w("project", "Could not load project.settings file", e);
+        }
     }
 
     public boolean hasGitSupport(){
@@ -46,5 +67,25 @@ public class Project {
 
     public Git getGit() {
         return git;
+    }
+
+    public boolean hasLastFile(){
+        return lastFile != null;
+    }
+
+    public File getLastFile(){
+        return lastFile;
+    }
+
+    /**
+     * IMPORTANT NOTE: the file passed MUST be within the project directory, otherwise this may do who knows what.
+     * @param file the file which was last opened
+     * @throws IOException if the config file could not be saved
+     */
+    public void setLastFile(File file) throws IOException {
+        lastFile = file;
+        prop.setProperty("lastFile", file.getAbsolutePath().substring(root.getAbsolutePath().length()));
+        FileOutputStream os = new FileOutputStream(new File(root, "project.settings"));
+        prop.storeToXML(os, "");
     }
 }
---
+++ app/src/main/java/uk/ac/tees/v8036651/mode/Projects/ProjectAdapter.java
@@ -0,0 +1,60 @@
+package uk.ac.tees.v8036651.mode.Projects;
+
+import android.content.Intent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+
+import uk.ac.tees.v8036651.mode.FileViewer.Screen_FileViewer;
+import uk.ac.tees.v8036651.mode.R;
+
+public class ProjectAdapter extends RecyclerView.Adapter<ProjectHolder>{
+
+    private ArrayList<Project> projects;
+    private RecyclerView recyclerView;
+
+    public ProjectAdapter(ArrayList<Project> projects){
+        this.projects = projects;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(@NonNull RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+        this.recyclerView = recyclerView;
+
+    }
+
+    @NonNull
+    @Override
+    public ProjectHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_project, parent, false);
+
+        ProjectHolder ph = new ProjectHolder(v);
+        return ph;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ProjectHolder holder, int position) {
+        holder.projectName.setText(projects.get(position).getName());
+        holder.projectPath.setText(projects.get(position).getRoot().getAbsolutePath());
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Project.openedProject = new Project(projects.get(position).getName(), projects.get(position).getRoot());
+                // TODO add ability to change directory in which file manager is opened
+                recyclerView.getContext().startActivity(new Intent(recyclerView.getContext(), Screen_FileViewer.class));
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return projects.size();
+    }
+}
