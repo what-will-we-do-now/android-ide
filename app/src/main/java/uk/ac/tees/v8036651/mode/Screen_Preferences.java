9beb6e138339453f2e35b7b242f2ebbcb6bc2221
DPBT7 <dominicpbthorp@gmail.com>
2020-03-20 14:34:06 +0000

    Trying to figure out why it is broke

--- app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Preferences.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Preferences.java
@@ -1,6 +1,8 @@
 package uk.ac.tees.v8036651.mode;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.View;
 
@@ -9,17 +11,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceClickListener;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class Screen_Preferences extends AppCompatActivity {
 
+    private static SharedPreferences.Editor prefEd;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        prefEd = getSharedPreferences("lightMode", MODE_PRIVATE).edit();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.settings, new SettingsFragment())
                 .commit();
+
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(false);
@@ -31,11 +40,31 @@ public class Screen_Preferences extends AppCompatActivity {
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
             Preference pattern = findPreference("pattern");
+            SwitchPreferenceCompat lightMode = findPreference("light_mode");
             OnPreferenceClickListener onClick = preference -> {
-                updatePattern();
+                if (preference.getKey() == "pattern"){
+                    updatePattern();
+                }
                 return true;
             };
+            Preference.OnPreferenceChangeListener onChange = (preference, newValue) -> {
+                if(preference.getKey() == "lightMode"){
+                    lightMode.setChecked((Boolean) newValue);
+                    if(lightMode.isChecked()){ updateTheme(getString(R.string.light_mode_enabled));}
+                    if(!lightMode.isChecked()){ updateTheme(getString(R.string.light_mode_enabled));}
+                }
+                return true;
+            };
+            lightMode.setOnPreferenceChangeListener(onChange);
             pattern.setOnPreferenceClickListener(onClick);
+        }
+
+        protected static void updateTheme(String summ){
+            prefEd.putString("lightMode",summ);
+            System.out.println("The summary is " + summ);
+            prefEd.commit();
+            System.out.println("Committed");
+
         }
 
         private void updatePattern() {
--- app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Splash.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/Screen_Splash.java
@@ -2,18 +2,27 @@ package uk.ac.tees.v8036651.mode;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentActivity;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreferenceCompat;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
+import android.widget.Switch;
 
 import java.io.File;
 
 public class Screen_Splash extends AppCompatActivity {
     private static int SPLASH_TIME_OUT = 1000;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        checkTheme();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.screen_splash);
 
@@ -21,7 +30,6 @@ public class Screen_Splash extends AppCompatActivity {
         if(!mainCodeDirectory.exists()){
             mainCodeDirectory.mkdir();
         }
-        checkTheme();
 
         new Handler().postDelayed(() -> {
             Intent splashIntent = new Intent(Screen_Splash.this, Screen_Login.class);
@@ -30,7 +38,18 @@ public class Screen_Splash extends AppCompatActivity {
         },SPLASH_TIME_OUT);
     }
 
-    public void checkTheme(){
-
+    protected void checkTheme(){
+        SharedPreferences pref = getSharedPreferences("lightMode", MODE_PRIVATE);
+        final String lmSumm = pref.getString("lightMode", "");
+        System.out.println("String value of Light Mode is: " + lmSumm);
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        if (lmSumm.equals(R.string.light_mode_disabled)){
+            setTheme(R.style.darkTheme);
+        }
+        else if (lmSumm.equals(R.string.light_mode_enabled)){
+            setTheme(R.style.lightTheme);
+        }
     }
 }
