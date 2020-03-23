323c3ecfff5c8f744c4009ed144abe04d20a94e6
vakus <sysojewosinskidominik@gmail.com>
2020-03-23 18:26:48 +0000

    Improved error handling and error messages displayed to user

--- app/src/main/java/uk/ac/tees/v8036651/mode/GitTools/GitPullTask.java
+++ app/src/main/java/uk/ac/tees/v8036651/mode/GitTools/GitPullTask.java
@@ -29,22 +29,22 @@ public class GitPullTask extends AsyncTask<Void, Integer, GitTaskStatus> {
             pull.call();
         } catch(TransportException e){
             Log.e("git", "Repository requires authentication", e);
-            return GitTaskStatus.REQUIRES_AUTHENTICATION;
+            return GitTaskStatus.GENERIC_REQUIRES_AUTHENTICATION;
         } catch (GitAPIException e) {
             Log.e("git", "Unable to pull", e);
-            return GitTaskStatus.FAILURE;
+            return GitTaskStatus.GENERIC_FAILURE;
         }
-        return GitTaskStatus.SUCCESS;
+        return GitTaskStatus.GENERIC_SUCCESS;
     }
 
     @Override
     protected void onPostExecute(GitTaskStatus status) {
         super.onPostExecute(status);
-        if(status.equals(GitTaskStatus.SUCCESS)){
+        if(status.equals(GitTaskStatus.GENERIC_SUCCESS)){
             Toast.makeText(context, context.getResources().getString(R.string.git_pull_message_successful), Toast.LENGTH_LONG).show();
-        }else if(status.equals(GitTaskStatus.FAILURE)){
+        }else if(status.equals(GitTaskStatus.GENERIC_FAILURE)){
             Toast.makeText(context, context.getResources().getString(R.string.git_pull_message_failure), Toast.LENGTH_LONG).show();
-        }else if(status.equals(GitTaskStatus.REQUIRES_AUTHENTICATION)){
+        }else if(status.equals(GitTaskStatus.GENERIC_REQUIRES_AUTHENTICATION)){
             GitTools.requestAuthentication(context, new Runnable() {
                 @Override
                 public void run() {
